b46b6b0562d3c68869d38e2e08c301d0616afb62
everton-dk9 <everyondk9@gmail.com>
2025-06-17 16:06:33 -0300

    estrutura de templates para o frontend e rotas basicas de teste

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', function () {
+    return view('pages.home');
+})->name('home');
+
+Route::get('/teste', function () {
+    return view('pages.teste');
+})->name('teste');
+
+Route::get('/welcome', function () {
     return view('welcome');
 });
