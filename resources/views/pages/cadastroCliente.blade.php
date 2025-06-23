7ca9d9fd8a4b322f4d8a40746a5a0f5d9afdfa71
Edvan-Teixeira <150725475+Edvan-Teixeira@users.noreply.github.com>
2025-06-23 17:01:45 -0300

    Update cadastroCliente.blade.php

--- resources/views/pages/cadastroCliente.blade.php
+++ resources/views/pages/cadastroCliente.blade.php
@@ -1,24 +1,46 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container p-4">
-    <main style="width: 100%; max-width: 330px; padding: 15px; margin: auto;">
-  <form>
-    <div class="form-floating">
-      <input type="nome" class="form-control" id="floatingNome">
-      <label for="floatingNome">Nome completo</label>
+<div class="container mt-3">
+        <form>
+            <div class="row">
+            <div class="col-6">
+            <div class="mb-3">
+                <label for="nomeFor" class="col-form-label">Nome</label>
+                <p class="border border-primary">
+                    <input type="nome" class="form-control" id="nomeFor" aria-describedby="nomeHelp">
+                </p>
+            </div>
+            </div>
+            <div class="col-6">
+            <div class="mb-3">
+                <label for="sobrenomeFor" class="col-form-label">Sobrenome</label>
+                <p class="border border-primary">
+                    <input type="sobrenome" class="form-control" id="sobrenomeFor" aria-describedby="sobrenomeHelp">
+                </p>
+            </div>
+            </div>
+            <div class="col-8">
+            <div class="mb-3">
+                <label for="InputEmail1" class="col-form-label">Email</label>
+                <p class="border border-primary">
+                    <input type="email" class="form-control" id="InputEmail1" aria-describedby="emailHelp">
+                </p>
+                    <div id="emailHelp" class="form-text">Email não será revelado.</div>
+            </div>
+            </div>
+            <div class="col-4">
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="col-form-label">Senha</label>
+                <p class="border border-primary">
+                    <input type="password" class="form-control" id="exampleInputPassword1">
+                </p>
+            </div>
+            </div>
+            </div>
+            <div class="mb-3">
+            <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </form>
     </div>
-    <div class="form-floating">
-      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-      <label for="floatingInput">Email</label>
-    </div>
-    <div class="form-floating">
-      <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-      <label for="floatingPassword">Senha</label>
-    </div>
-
-    <button class="w-100 btn btn-lg btn-primary" type="submit">Cadastrar</button></li>
-  </form>
-    </div>
-</main>
 @endsection
