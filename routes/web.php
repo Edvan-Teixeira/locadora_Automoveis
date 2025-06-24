9944e8fd3f65ed6857bc78b445c9612c5fd6a18b
everton-dk9 <everyondk9@gmail.com>
2025-06-24 12:38:53 -0300

    feature(rotas e paginas): configuração de rotas de autenticação e paginas de teste para login/registro e logout

--- routes/web.php
+++ routes/web.php
@@ -22,13 +22,37 @@ Route::get('/clientes', function () {
 });
 Route::get('/singIN', function () {
 Route::get('/', function () {
-    return view('pages.singIN');
-})->name('singIN');
-
-Route::get('/cadasrtoCliente', function () {
-    return view('pages.cadastroCliente');
-})->name('cadastroCliente');
-
-Route::get('/welcome', function () {
     return view('welcome');
 })->name('welcome');
+
+Route::get('/teste', function () {
+    return view('pages.teste');
+})->name('teste');
+
+Route::middleware('guest')->group( function () {
+    Route::get('/account/register', [AuthController::class, 'showRegistrationForm'])->name('register');
+    Route::post('/account/register', [AuthController::class, 'register']);
+
+    Route::get('/account/login', [AuthController::class, 'showLoginForm'])->name('login');
+    Route::post('/account/login', [AuthController::class, 'authenticate']);
+});
+
+Route::middleware('auth')->group( function () {
+    Route::get('/home', function () {
+        return view('pages.home');
+    })->name('home');
+
+    Route::get('/account/logout', [AuthController::class, 'logout'])->name('logout');
+
+    Route::get('/clientes', function () {
+        return view('pages.clientes');
+    })->name('clientes');
+
+    Route::get('/', function () {
+        return view('pages.singIN');
+    })->name('singIN');
+
+    Route::get('/cadasrtoCliente', function () {
+        return view('pages.cadastroCliente');
+    })->name('cadastroCliente');
+});
